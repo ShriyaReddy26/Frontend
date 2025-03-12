51f48f3df71eb51965a545889c99d45e62a46204
ShriyaReddy26 <shriyareddy2629@gmail.com>
2025-03-12 09:48:44 +0530

    shriy

--- promise.js
+++ promise.js
@@ -1,15 +1,26 @@
-const mypromise = new Promise((resolve,reject) => {
-    setTimeout(() => {
-        resolve("hello this is resolved")
-    },2000)
+// const mypromise = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         resolve("hello this is resolved")
+//     },2000)
+// })
+
+// mypromise
+//   .then((message)=>{
+//     console.log(message + "full");       
+//    })
+//   .catch((error)=>{
+//     console.log(error + " reject");
+//    })
+    
+const mypromise = new Promise((resolve,reject)=>{
+    const randomNum = Math.random();
+    if(randomNum>0.5){
+        resolve("Numer greater")
+    }else{
+        reject("smaller")
+    }
 })
 
 mypromise
-  .then((message)=>{
-    console.log(message + "full");       
-   })
-  .catch((error)=>{
-    console.log(error + " reject");
-   })
-    
-
+ .then((value)=>{console.log(value)})
+ .catch((error)=>{console.log(error)})
