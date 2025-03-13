ad98566bf3ac400aad9369582353e85343b55135
ShriyaReddy26 <shriyareddy2629@gmail.com>
2025-03-13 09:55:40 +0530

    shriy

--- js/advancepromise.js
+++ js/advancepromise.js
@@ -1,5 +1,34 @@
+// let promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("First"),1000);
+// })
+// let promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Second"),2000);
+// })
+// let promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Third"),3000);
+// })
+
+// Promise.all([promise1,promise2,promise3])
+// .then((value)=>{console.log(value)})
+// .catch((error)=>{console.log(error)})
+
+// let promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("First"),1000);
+// })
+// let promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Second"),2000);
+// })
+// let promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Third"),3000);
+// })
+
+// Promise.allSettled([promise1,promise2,promise3])
+// .then((value)=>{console.log(value)})
+// .catch((error)=>{console.log(error)})
+
+
 let promise1 = new Promise((resolve,reject)=>{
-    setTimeout(resolve("First"),1000);
+    setTimeout(reject("First"),1000);
 })
 let promise2 = new Promise((resolve,reject)=>{
     setTimeout(resolve("Second"),2000);
@@ -8,6 +37,6 @@ let promise3 = new Promise((resolve,reject)=>{
     setTimeout(resolve("Third"),3000);
 })
 
-Promise.all([promise1,promise2,promise3])
+Promise.any([promise1,promise2,promise3])
 .then((value)=>{console.log(value)})
 .catch((error)=>{console.log(error)})
