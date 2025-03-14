73b398154e965a7f9b39a4c67ff43676f8b92cb9
ShriyaReddy26 <shriyareddy2629@gmail.com>
2025-03-14 09:39:07 +0530

    shriy

--- js/advancepromise.js
+++ js/advancepromise.js
@@ -49,17 +49,31 @@
 
 
 // Promise.race...............................................
-let promise1 = new Promise((resolve,reject)=>{
-    setTimeout(reject("First"),1000);
-})
-let promise2 = new Promise((resolve,reject)=>{
-    setTimeout(resolve("Second"),2000);
-})
-let promise3 = new Promise((resolve,reject)=>{
-    setTimeout(resolve("Third"),3000);
-})
+// let promise1 = new Promise((resolve,reject)=>{
+//     setTimeout(reject("First"),1000);
+// })
+// let promise2 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Second"),2000);
+// })
+// let promise3 = new Promise((resolve,reject)=>{
+//     setTimeout(resolve("Third"),3000);
+// })
 
-Promise.race([promise1,promise2,promise3])
-.then((value)=>{console.log(value)})
-.catch((error)=>{console.log(error)})
+// Promise.race([promise1,promise2,promise3])
+// .then((value)=>{console.log(value)})
+// .catch((error)=>{console.log(error)})
 
+
+function addSum(){
+    let s = 0
+    for (let i = 0 ; i<=100000;i++){
+        s+=1
+    }
+    return s
+}
+
+let start = performance.now()
+let sum = addSum()
+let end = performance.now()
+
+console.log(end-start)
